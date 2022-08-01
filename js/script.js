9e452700f98bb72e6ee9966a644aa3d2809594de
simon <symon261731@gmail.com>
2022-08-01 21:04:55 +0500

    add new task sumtwoSmallestNumbers

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 function printerError(s){
     let sum = 0;
-    for( l of s ){
+    for(let l of s ){
         if (l =='a' || l == 'b' || l == 'c' || l== 'd' || l == 'e' || l == 'f' || l == 'g' || l == 'h' || l == 'i' || l == 'j' || l == 'k' || l == 'k' || l == 'l' || l =='m'){
         sum +=0;
         }else{
@@ -12,3 +12,15 @@ function printerError(s){
 }
 
 console.log(printerError("aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz"));
+
+function sumTwoSmallestNumbers(numbers){
+    numbers.sort( function(a,b){
+        return a-b;
+    });
+    console.log(numbers);
+    let sum;
+    sum = numbers[0]+ numbers[1];
+    console.log(sum);
+
+}
+sumTwoSmallestNumbers([52, 76, 14, 12, 4]);

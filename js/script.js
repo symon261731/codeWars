f24278b17ed02c842351380b4a8bca488d918cd4
Simon Mishchenko <symon261731@gmail.com>
2022-08-11 03:27:32 +0500

    add new homework for wednesday for bootcamp

--- js/script.js
+++ js/script.js
@@ -393,3 +393,148 @@ const calcMultiply = (arr) =>{
 };
 console.log(calcMultiply([1,5,6,65,3,6,8,4,1,3,67,12,257,2,5,12]));//проблема с последний элементом
 ///////////////////////////////////////////
+//MUMBLING
+
+
+
+
+///////////////////////////////
+let plitByLength = (str, l) => {
+    let arr = str.split('');
+    let result = [];
+    for (let i = 0; i < arr.length; i = +l) {
+      result.push(arr.slice(0, l));
+      arr.splice(0, l);
+    }
+    result = result.map((item) => {
+      return item.join('');
+    });
+    return result;
+  };
+  console.log(plitByLength('hello there Obi-wan Kenobi', 4));
+
+
+
+
+let splitByLength = (str ,sliceLength) => {
+    const arr =[];
+    for( let i=0; i<str.length; i+=sliceLength){
+        arr.push( str.slice(i, i+=sliceLength));
+    }
+    return arr;
+  };
+  console.log(splitByLength('hello there Obi-wan Kenobi', 4));
+
+  
+  /////////////////////////////////
+  
+
+  function padIt(str,n){
+    let s = str;
+    for( let i=0; i<n;i++){
+        if( i%2==0){
+            s = '*' + s;
+        }else{
+            s = s+'*';
+        }
+    }
+    console.log(s);
+  }
+  padIt('a',1);
+  padIt('a',3);
+  padIt('a',8);
+
+
+  function id(str,n){
+    let s = str;
+    let i=0;
+    while(i<n){
+        if(i%2){
+            s = s+'*';
+        } else{
+            s = '*'+s;
+        }
+        i++;
+    }
+  }
+///////////////////////////////////////////////////////////////////
+
+function z(s){
+    let str = '';
+    for( let i=0;i<s.length;i++){
+        str+= s[i].toUpperCase();
+        for(let j=0; j<i;j++){
+            str+=  s[i].toLowerCase();          
+        }
+        str+='-';
+    }
+    str = str.slice(0,-1);
+    console.log(str);
+}
+z("ZpglnRxqenU");
+
+//////////////////////////////////////////////////
+
+
+let count = (array,elem) =>{
+    let sum =0;
+    let arr =[]
+    array.forEach((item,i) =>{
+        if( item == elem){
+            sum+= 1;
+            arr.push(i);
+        }
+    });
+    return `Element ${elem} found out ${sum} times \n index of element ${arr}`;
+};
+
+console.log(count([1,2,2,6,7,9,5,34,2],2));
+//////////////////////////////////////////////////
+
+let showIndex = (array)=>{
+    let newArray = [];
+    for(let i =0; i<array.length; i++){
+        newArray.push(array.indexOf(i));
+    }
+    return newArray;
+};
+
+console.log(showIndex([2,3,4,5,6,7,8,9,10,11,2,3,4,5,6,7,11]));
+/////////////////////////////////////////////
+
+let removeDuplicateElem = (array) =>{
+    let newArr =[];
+    array.forEach((item,index)=>{
+        if(array.indexOf(item) == index){
+            newArr.push(item);   
+        }
+    });
+    return newArr;
+};
+console.log(removeDuplicateElem([2,3,4,5,6,7,8,9,10,11,2,3,4,5,6,7,11]));
+
+let removeDuplicateElem2 = (array) =>{
+    let newArr = [];
+    for( let i=0 ;i<array.length;i++){
+        if( array.indexOf(array[i]) == i){
+            newArr.push(array[i]);
+        }
+    }
+    return newArr;
+};
+console.log(removeDuplicateElem2([2,3,4,5,6,7,8,9,10,11,2,3,4,5,6,7,11]));
+
+let removeDuplicateElem3 = (array) => array.filter((item,index)=> array.indexOf(item)==index );
+console.log(removeDuplicateElem3([2,3,4,5,6,7,8,9,10,11,2,3,4,5,6,7,11]));
+
+
+
+/////////////////////////////////////////////////
+
+let trimSpaces = str =>{
+   let newStr=[];
+   return newStr = str.split('').filter(item=> item!=' ').join(''); 
+};
+console.log(trimSpaces('Ко с мос'));
+
+/////////////////////////////////////////////////////
